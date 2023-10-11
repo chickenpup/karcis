f7f1753eb0df20da16fbc4ea5ab1a913cb547681
Your Name <--unset>
2023-10-11 11:28:05 +0700

    Menambahkan validasi jika tidak ada session maka tidak bisa melihat list tiket

--- function/getTickets.php
+++ function/getTickets.php
@@ -1,13 +1,15 @@
 <?php
-    @session_start();
-    
-    $id = @$_SESSION['id'];
-   
-    if(!$id){
-        header('location:'.$host.'signin.php');
-    }
+@session_start();
 
+$id = @$_SESSION['id'];
+
+if (!$id) {
+    // header('location: ../home.php');
+    exit;
+} else {
     $sql = "SELECT * FROM tickets";
     $result = $conn->query($sql);
+}
+
 
 ?>
