711ff65b163e39dd2a1bc58bec18ee03ef4454cb
AndikLaksonoAgit <53463371+AndikLaksonoAgit@users.noreply.github.com>
2023-10-11 11:55:01 +0700

    patch andik validasi signup

--- function/actSignup.php
+++ function/actSignup.php
@@ -3,13 +3,19 @@
 
     $fullname = @$_POST['fullname'];
     $email    = @$_POST['email'];
-    $password  = sha1(@$_POST['password']);
-
+    //ganti dengan password Bcrypt yg lebih aman , Andik
+    //$password  = sha1(@$_POST['password']);
+    $password  = password_hash(@$_POST['password'], PASSWORD_BCRYPT);
+    
+    //tambahan htmlentities , utk mengatasi sqlinjection , Andik
+    $fullnameClean = htmlentities($fullname);
+    $emailClean = htmlentities($email);
+    
     // insert to database
-    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+    $sql = "INSERT INTO users (email, password) VALUES ('$emailClean', '$password')";
 
     if ($conn->query($sql) === TRUE) {
-        $sql_profile = "INSERT INTO user_profile (id_user,fullname) VALUES ('$conn->insert_id','$fullname')";
+        $sql_profile = "INSERT INTO user_profile (id_user,fullname) VALUES ('$conn->insert_id','$fullnameClean')";
         if($conn->query($sql_profile) === TRUE){
             
             header('location:'.$host.'signin.php?status=success');
